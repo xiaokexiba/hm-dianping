7eba7602d98393ba57d1c4f2667fb8d0654627d6
xiaoke <yjmqaz@126.com>
2022-11-17 20:50:37 +0800

    22/11/17

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -2,17 +2,32 @@ package com.hmdp.service.impl;
 
 import cn.hutool.core.util.BooleanUtil;
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONTokener;
 import cn.hutool.json.JSONUtil;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.CacheClient;
+import com.hmdp.utils.RedisData;
+import com.hmdp.utils.SystemConstants;
+import com.sun.org.apache.bcel.internal.generic.LCONST;
+import org.springframework.data.geo.Distance;
+import org.springframework.data.geo.GeoResult;
+import org.springframework.data.geo.GeoResults;
+import org.springframework.data.redis.connection.RedisGeoCommands;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.domain.geo.GeoReference;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.time.LocalDateTime;
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 import static com.hmdp.utils.RedisConstants.*;
@@ -31,129 +46,39 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
     @Resource
     private StringRedisTemplate stringRedisTemplate;
 
-    /**
-     * 根据ID查询数据
-     *
-     * @param id 商品ID
-     * @return 统一返回类
-     */
+    @Resource
+    private CacheClient cacheClient;
+
     @Override
     public Result queryById(Long id) {
-        // 缓存穿透
-        // Shop shop = queryWithPassThrough(id);
+        // 解决缓存穿透
+        Shop shop = cacheClient
+                .queryWithPassThrough(CACHE_SHOP_KEY, id, Shop.class, this::getById, CACHE_SHOP_TTL, TimeUnit.MINUTES);
 
         // 互斥锁解决缓存击穿
-        Shop shop = queryWithMutex(id);
+        // Shop shop = cacheClient.queryWithMutex(CACHE_SHOP_KEY, id, Shop.class, this::getById, CACHE_SHOP_TTL, TimeUnit.MINUTES);
+
+        // 逻辑过期解决缓存击穿
+        // Shop shop = cacheClient.queryWithLogicalExpire(CACHE_SHOP_KEY, id, Shop.class, this::getById, 20L, TimeUnit.SECONDS);
+
         if (shop == null) {
             return Result.fail("店铺不存在！");
         }
+        // 7.返回
         return Result.ok(shop);
     }
 
-    public Shop queryWithMutex(Long id) {
-        String key = CACHE_SHOP_KEY + id;
-        // 从redis中查询数据
-        String jsonShop = stringRedisTemplate.opsForValue().get(key);
-        // 存在，返回信息
-        if (StrUtil.isNotBlank(jsonShop)) {
-            return JSONUtil.toBean(jsonShop, Shop.class);
-        }
-        if (jsonShop != null) {
-            return null;
-        }
-        String lockKey = null;
-        Shop shop = null;
-        try {
-            // 实现缓存重建
-            // 获取互斥锁
-            lockKey = LOCK_SHOP_KEY + id;
-            boolean isLock = tryLock(lockKey);
-            // 判断是否获取成功
-            if (!isLock) {
-                // 失败，休眠并进行重试
-                Thread.sleep(50);
-                return queryWithPassThrough(id);
-            }
-            // 不存在，在数据中查询
-            shop = getById(id);
-            // 模拟重建的延迟
-            Thread.sleep(200);
-            if (shop == null) {
-                // 将空值写入redis
-                stringRedisTemplate.opsForValue().set(key, "", CACHE_NULL_TTL, TimeUnit.MINUTES);
-                // 不存在，返回错误信息
-                return null;
-            }
-            // 存在，将信息写入redis中，并且返回信息
-            stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop), CACHE_SHOP_TTL, TimeUnit.MINUTES);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        } finally {
-            // 释放互斥锁
-            unLock(lockKey);
-        }
-        return shop;
-    }
-
-    public Shop queryWithPassThrough(Long id) {
-        String key = CACHE_SHOP_KEY + id;
-        // 从redis中查询数据
-        String jsonShop = stringRedisTemplate.opsForValue().get(key);
-        // 存在，返回信息
-        if (StrUtil.isNotBlank(jsonShop)) {
-            return JSONUtil.toBean(jsonShop, Shop.class);
-        }
-        if (jsonShop != null) {
-            return null;
-        }
-        // 不存在，在数据中查询
-        Shop shop = getById(id);
-        if (shop == null) {
-            // 将空值写入redis
-            stringRedisTemplate.opsForValue().set(key, "", CACHE_NULL_TTL, TimeUnit.MINUTES);
-            // 不存在，返回错误信息
-            return null;
-        }
-        // 存在，将信息写入redis中，并且返回信息
-        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop), CACHE_SHOP_TTL, TimeUnit.MINUTES);
-        return shop;
-    }
-
-    /**
-     * 更新店铺信息
-     *
-     * @param shop 店铺
-     * @return 统一返回类
-     */
     @Override
     @Transactional
     public Result update(Shop shop) {
         Long id = shop.getId();
         if (id == null) {
-            return Result.fail("店铺ID不能为空");
+            return Result.fail("店铺id不能为空");
         }
-        // 更新数据库
+        // 1.更新数据库
         updateById(shop);
-        // 删除缓存
+        // 2.删除缓存
         stringRedisTemplate.delete(CACHE_SHOP_KEY + id);
         return Result.ok();
-    }
-
-    /**
-     * 上锁
-     */
-    private boolean tryLock(String key) {
-        Boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(key, "xxx", 10, TimeUnit.SECONDS);
-        // 这里直接返回flag的话，会进行拆箱
-        // 拆箱底层就是调用booleanValue()方法，如果flag为null的话就会空指针异常
-        // 所以这里直接用工具类进行拆箱
-        return BooleanUtil.isTrue(flag);
-    }
-
-    /**
-     * 开锁
-     */
-    private void unLock(String key) {
-        stringRedisTemplate.delete(key);
     }
 }
--- src/main/java/com/hmdp/utils/RedisData.java
+++ src/main/java/com/hmdp/utils/RedisData.java
@@ -4,6 +4,9 @@ import lombok.Data;
 
 import java.time.LocalDateTime;
 
+/**
+ * @author xoke
+ */
 @Data
 public class RedisData {
     private LocalDateTime expireTime;
